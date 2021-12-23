b19664717813da81b61b2c24050a812c3634eea3
Bharath <56950806+bablubharath@users.noreply.github.com>
2021-12-23 16:34:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,3 +81,14 @@ data "aws_s3_bucket_object" "default" {
   bucket = var.s3_bucket
   key    = var.s3_key_path #basename ("{var.s3_key_path}")
 }
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = var.s3_bucket
+  key    = var.s3_key_path
+  source = var.source_file_path
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(var.source_file_path)
+}
